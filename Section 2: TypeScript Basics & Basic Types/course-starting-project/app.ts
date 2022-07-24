7a65e160535b4c7e66301d640d48d20993866db8
adclleva <arvin.lleva@yahoo.com>
2022-07-23 23:05:47 -0700

    array types

--- Section 2: TypeScript Basics & Basic Types/course-starting-project/app.ts
+++ Section 2: TypeScript Basics & Basic Types/course-starting-project/app.ts
@@ -1,10 +1,21 @@
 // specialized object type
-const person: {
-  name: string;
-  age: number;
-} = {
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
+// infers the object types
+const person = {
   name: "Arvin",
   age: 28,
+  hobbies: ["Sports", "Cooking"],
 };
 
+let favoriteActivites: string[];
+favoriteActivites = ["Sports"];
+
 console.log(person.age);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+  // console.log(hobby.map()); // !! ERROR
+}
