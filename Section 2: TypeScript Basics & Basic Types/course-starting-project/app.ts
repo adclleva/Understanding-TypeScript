1a9a541d0492cbe0283ca9db889b260190838877
Arvin Lleva <arvinlleva@HE00629-MBP.lan>
2022-07-31 00:05:10 -0700

    learned enums

--- Section 2: TypeScript Basics & Basic Types/course-starting-project/app.ts
+++ Section 2: TypeScript Basics & Basic Types/course-starting-project/app.ts
@@ -4,20 +4,32 @@
 //   age: number;
 // } = {
 // infers the object types
-const person: {
-  name: string;
-  age: number;
-  hobbies: string[];
-  role: [number, string]
-}= {
+
+// const ADMIN = 0 
+// const READ_ONLY = 1
+// const AUTHOR = 2
+
+// custom type
+// you can have no numbers and it would increment
+enum Role { ADMIN = 'ADMIN', READ_ONLY = 100, AUTHOR = 'AUTHOR' }
+
+const person
+// : {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string]
+// } 
+= {
   name: "Arvin",
   age: 28,
   hobbies: ["Sports", "Cooking"],
-  role: [2, "author"]
+  // role: [2, "author"]
+  role: Role.ADMIN
 };
 
 // TS doesn't catch tuples when pushing items more than once
-person.role.push('admin')
+// person.role.push('admin')
 // person.role[1]= 10;
 
 let favoriteActivites: string[];
@@ -28,4 +40,8 @@ console.log(person.age);
 for (const hobby of person.hobbies) {
   console.log(hobby.toUpperCase());
   // console.log(hobby.map()); // !! ERROR
+}
+
+if (person.role === Role.ADMIN) {
+ console.log('is admin') 
 }
