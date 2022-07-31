a462e76019f05180ce4a9d997033f1eda76e40d6
Arvin Lleva <arvinlleva@HE00629-MBP.lan>
2022-07-30 23:25:53 -0700

    learned about tuples

--- Section 2: TypeScript Basics & Basic Types/course-starting-project/app.ts
+++ Section 2: TypeScript Basics & Basic Types/course-starting-project/app.ts
@@ -4,11 +4,21 @@
 //   age: number;
 // } = {
 // infers the object types
-const person = {
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string]
+}= {
   name: "Arvin",
   age: 28,
   hobbies: ["Sports", "Cooking"],
+  role: [2, "author"]
 };
+
+// TS doesn't catch tuples when pushing items more than once
+person.role.push('admin')
+// person.role[1]= 10;
 
 let favoriteActivites: string[];
 favoriteActivites = ["Sports"];
