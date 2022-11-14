3c59206daf5483058b0d3956c29e5d5e87c798b7
Arvin Lleva <arvin.lleva@yahoo.com>
2022-11-13 23:24:36 -0800

    created the third section of the ts compiler project and set up the tsconfig file

---
+++ Section 3: The TypeScript Compiler (and its Configuration)/course-starting-project/app.ts
@@ -0,0 +1,13 @@
+// doing `tsc app.ts -w` within the terminal when in the directory watches the file through the tsc compiler
+
+const userName = "Arvin"
+
+console.log(userName)
+
+// notes:
+
+/**
+ * tsc --init
+ * the command above enables to initialize the typescript compilation for the project and enables us
+ * to compile multiple files
+ */
