f0c812f23cc0eaa4007710b21fdb943366d15bc2
Arvin Lleva <arvin.lleva@yahoo.com>
2022-11-13 23:40:54 -0800

    understanding ts core libs

--- Section 3: The TypeScript Compiler (and its Configuration)/course-starting-project/app.ts
+++ Section 3: The TypeScript Compiler (and its Configuration)/course-starting-project/app.ts
@@ -11,3 +11,15 @@ console.log(userName)
  * the command above enables to initialize the typescript compilation for the project and enables us
  * to compile multiple files
  */
+
+
+/**
+ * ts doesn't complain about the code below because not only the ! but also the target and module and lib settings within the 
+ * tsconfi.json file within the project.
+ * since lib is commented out, it uses the default given by the target settings which is why the error is not showing below
+ */
+const button = document.querySelector('button')!;
+
+button.addEventListener('click', () => {
+  console.log('clicked!')
+})
